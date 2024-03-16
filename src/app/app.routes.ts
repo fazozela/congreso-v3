043246faf4453ffd2c85f5cf19df49353e1cea57
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-03-15 22:52:03 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/home/home.component')
+  },
+  {
+    path: 'convocatoria',
+    loadComponent: () => import('./pages/announcement/announcement.component')
+  },
+  {
+    path: 'inscripcion',
+    loadComponent: () => import('./pages/enrollment/enrollment.component')
+  },
+  {
+    path: 'programa',
+    loadComponent: () => import('./pages/schedule/schedule.component')
+  },
+  {
+    path: 'expositores',
+    loadComponent: () => import('./pages/speakers/speakers.component')
+  },
+  {
+    path: 'organizadores',
+    loadComponent: () => import('./pages/organizers/organizers.component')
+  },
+  {
+    path: 'archivo',
+    loadComponent: () => import('./pages/archive/archive.component')
+  },
+  { path: '**', redirectTo: 'inicio' },
+];
---
+++ src/app/pages/organizers/organizers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizers',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizers.component.html',
+  styleUrl: './organizers.component.css'
+})
+export default class OrganizersComponent {
+
+}
