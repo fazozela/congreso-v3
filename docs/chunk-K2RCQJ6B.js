c999a94d7e75cc54037972a87bead5a2591d2c73
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-04-04 00:10:31 -0400

    put to test pages

--- docs/chunk-K2RCQJ6B.js
+++
@@ -1,1 +0,0 @@
-import{Da as i,Ea as s,O as o,wa as a,xa as r}from "./chunk-6X4RNDUB.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-organizers"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n, c){n&1&&(a(0,"p"),i(1,"organizers works!"),r())}});let t=e;return t})();export{f as default};
--- docs/chunk-O4CUWP3O.js
+++
@@ -1,1 +0,0 @@
-import{Da as r,Ea as s,O as o,wa as a,xa as i}from "./chunk-6X4RNDUB.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-schedule"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n, d){n&1&&(a(0,"p"),r(1,"schedule works!"),i())}});let t=e;return t})();export{p as default};
